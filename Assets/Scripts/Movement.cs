0a681c76cf4a6d73185aca73cf0e4d3a0577f763
EE539 <56602339+EE539@users.noreply.github.com>
2022-07-05 19:32:02 +0300

    Klavye ve mouse kaldırıldı, sadece dokunmatik özellik kaldı. Durdurucular düzgün çalışıyor

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,42 +6,24 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speedOfTheGun = 10f;
-    private float playerMovement, touchMovement;
-    private Vector2 mouseMovement;
+    private float touchMovement;
 
     public InputActionAsset Map;
     InputActionMap gameplay;
-    InputAction playerHorizontalKeyboardInput, playerHorizontalMouseInput, playerHorizontalTouchInput, playerHorizontalTouchSwipeInput;
+    InputAction playerHorizontalTouchInput, playerHorizontalTouchSwipeInput;
     // Start is called before the first frame update
     void Awake()
     {
         gameplay = Map.FindActionMap("Player");
-        playerHorizontalKeyboardInput = gameplay.FindAction("Keyboard");
-        playerHorizontalMouseInput = gameplay.FindAction("Mouse");
         playerHorizontalTouchInput = gameplay.FindAction("TouchPress");
         playerHorizontalTouchSwipeInput = gameplay.FindAction("TouchSwipe");
 
-        playerHorizontalKeyboardInput.performed += HorizontalKeyboardInput;
-        playerHorizontalMouseInput.performed += HorizontalMouseInput;
         playerHorizontalTouchInput.started += TouchInput;
         playerHorizontalTouchSwipeInput.performed += HorizontalTouchInput;
 
-        playerHorizontalKeyboardInput.canceled += HorizontalInputCancel;
         playerHorizontalTouchInput.canceled += TouchInputCanceled;
     }
 
-    
-
-    private void HorizontalKeyboardInput(InputAction.CallbackContext obj)
-    {
-        playerMovement = obj.ReadValue<float>();
-        Debug.Log("Player Movement performed= " + playerMovement);
-    }
-
-    private void HorizontalMouseInput(InputAction.CallbackContext obj)
-    {
-        mouseMovement = obj.ReadValue<Vector2>();
-    }
 
     private void TouchInput(InputAction.CallbackContext obj)
     {
@@ -60,22 +42,15 @@ public class Movement : MonoBehaviour
         Debug.Log("Touch finished ");
     }
     
-    private void HorizontalInputCancel(InputAction.CallbackContext obj)
-    {
-        playerMovement = 0;
-        Debug.Log("Player Movement canceled= " + playerMovement);
-    }
     private void OnEnable()
     {
-        playerHorizontalKeyboardInput.Enable();
-        playerHorizontalMouseInput.Enable();
+       
         playerHorizontalTouchInput.Enable();
         playerHorizontalTouchSwipeInput.Enable();
     }
     private void OnDisable()
     {
-        playerHorizontalKeyboardInput.Disable();
-        playerHorizontalMouseInput.Disable();
+        
         playerHorizontalTouchInput.Disable();
         playerHorizontalTouchSwipeInput.Disable();
     }
@@ -91,10 +66,15 @@ public class Movement : MonoBehaviour
     void FixedUpdate()
     {
         float move = 0, rotate = 0;
-        //rotate = playerMovement * Time.deltaTime;
-        //rotate = (-mouseMovement.x / 100) * Time.deltaTime;
+        
         move = -speedOfTheGun * Time.deltaTime;
         rotate = -(touchMovement/10) * Time.deltaTime;
         transform.Translate(0, rotate, move);
+        if (transform.position.x >= 0.655f)
+            transform.position = new Vector3(0.655f, transform.position.y, transform.position.z);
+        else if (transform.position.x <= -0.627f)
+            transform.position = new Vector3(-0.627f, transform.position.y, transform.position.z);
+        if (transform.position.z >= 86)
+            transform.position = new Vector3(transform.position.x, transform.position.y, 86);
     }
 }
