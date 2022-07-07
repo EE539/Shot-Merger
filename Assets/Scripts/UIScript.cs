a3ee77ecca1af12f37a997a44612ca329daf357f
EE539 <56602339+EE539@users.noreply.github.com>
2022-07-07 19:22:22 +0300

    kendi kendine hareket eden slider eklendi

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class UIScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float timeToWait = 0.5f, fillQuantity = 0.1f;
+    private int count = 0;
+    public Slider sliderValue;
+    public void RestartButton()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; //Get the index of the scene
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    float CalculateSliderValue()
+    {
+        if(count == 0)
+        {
+            if (sliderValue.value == 1)
+                count = 1;
+            return sliderValue.value + Time.deltaTime;
+        }
+        else
+        {
+            if (sliderValue.value == 0)
+                count = 0;
+            return sliderValue.value - Time.deltaTime;
+        }
+            
+       
+    }
+    private void Update()
+    {
+        sliderValue.value = CalculateSliderValue();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
